2e445053144400b82b128ccb04f34ba29a09316b
Mac Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2019-04-25 18:36:09 +0300

    Added test ui

--- 01_ATM/lib/nice_bank.rb
+++ 01_ATM/lib/nice_bank.rb
@@ -28,3 +28,24 @@ class CashSlot
     @contents = amount_to_dispense
   end
 end
+
+require 'sinatra'
+get '/' do
+  %{
+    <html>
+      <body>
+        <form action="/withdraw" method="post">
+          <label for="amount">Amount</label>
+          <input type="text" id="amount" name="amount">
+          <button type="submit">Withdraw</button>
+        </form>
+      </body>
+    </html>
+  }
+end
+
+set :cash_slot, CashSlot.new
+post '/withdraw' do
+  teller = Teller.new(settings.cash_slot)
+  fail "I don't know how to withdraw yet, sorry"
+end
