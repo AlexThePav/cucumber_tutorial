a6954489be52ea400c36486f6f20dd65b17d25b8
pav-thinkpad-w520 <alexandrupavilcu@gmail.com>
2019-05-01 18:50:17 +0300

    added activerecord account so dont need balance_store anymore

--- 01_ATM/lib/nice_bank.rb
+++ 01_ATM/lib/nice_bank.rb
@@ -1,24 +1,24 @@
 require_relative 'transaction_queue'
 require_relative 'balance_store'
 
-class Account
-  def initialize
-    @queue = TransactionQueue.new
-    @balance_store = BalanceStore.new
-  end
+# class Account
+#   def initialize
+#     @queue = TransactionQueue.new
+#     @balance_store = BalanceStore.new
+#   end
 
-  def credit(amount)
-    @queue.write("+#{amount}")
-  end
+#   def credit(amount)
+#     @queue.write("+#{amount}")
+#   end
 
-  def balance
-    @balance_store.balance
-  end
+#   def balance
+#     @balance_store.balance
+#   end
 
-  def debit(amount)
-    @queue.write("-#{amount}")
-  end
-end
+#   def debit(amount)
+#     @queue.write("-#{amount}")
+#   end
+# end
 
 class Teller
   def initialize(cash_slot)
