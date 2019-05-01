646b8ae56c4cf89bdd4a569f797467dbd924f25e
pav-thinkpad-w520 <alexandrupavilcu@gmail.com>
2019-05-01 22:05:40 +0300

    tried to make activerecord work

--- 01_ATM/lib/account.rb
+++ 01_ATM/lib/account.rb
@@ -1,8 +1,19 @@
 require 'active_record'
+# task :up => :'db:connect' do
+#   migrations = if ActiveRecord.version.version >= '5.2'
+#     ActiveRecord::Migration.new.migration_context.migrations
+#   else
+#     ActiveRecord::Migrator.migrations('db/migrate')
+#   end
+#   ActiveRecord::Migrator.new(:up, migrations, nil).migrate
+# end
+
+
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3',
                                         :database => 'db/bank.db')
-ActiveRecord::Migrator.migrate("db/migrate/")
-
+migrations = ActiveRecord::Migration.new.migration_context.migrations
+  ActiveRecord::Migrator.new(:up, migrations, nil).migrate
+  
 class Account < ActiveRecord::Base
   validates_uniqueness_of :number
   
--- 01_ATM/lib/transaction_processor.rb
+++ 01_ATM/lib/transaction_processor.rb
@@ -2,12 +2,14 @@ require_relative 'account'
 require_relative 'transaction_queue'
 
 transaction_queue = TransactionQueue.new
-balance_store = BalanceStore.new
 puts "transaction processor ready"
 loop do
   transaction_queue.read do |message|
-    transaction_amount = message.to_i
-    new_balance = balance_store.balance + transaction_amount
-    balance_store.balance = new_balance
+    sleep 1
+    transaction_amount, number = message.split(/,/)
+    account = Account.find_by_number!(number.strip)
+    new_balance = account.balance + transaction_amount.to_i
+    account.balance = new_balance
+    account.save
   end
 end
